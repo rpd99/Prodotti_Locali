bb80a77013da3b8b03da21728763be75b7256c28
sampei98 <46533982+sampei98@users.noreply.github.com>
2019-06-17 16:36:22 +0200

    Update ecommerce.sql

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,49 +3,33 @@ CREATE database ecommerce;
 USE ecommerce; 
 
 CREATE TABLE utente (
-	email varchar(50) not null primary key, 
+    email varchar(50) not null primary key, 
+    nome VARCHAR(20) NOT NULL,
+    cognome VARCHAR(20) NOT NULL,
     telefono varchar(15) not null, 
     cap char(5) not null, 
     via varchar(30) not null, 
     n_civico int not null, 
     nazione varchar(25) not null,
     citta varchar(50) not null,
-    password_utente varchar(30) not null
+    password_utente varchar(30) not null,
+    isAdmin tinyint(1)
 );
 
-INSERT INTO utente (email, telefono, cap, via, n_civico, nazione, citta, password_utente) 
-VALUES ('a@gmail.com', '3384255568', '83040', 'via piani', 22, 'italia', 'roma', 'aaa'); 
+INSERT INTO utente (email,nome,cognome, telefono, cap, via, n_civico, nazione, citta, password_utente,isAdmin) 
+VALUES ('a@gmail.com',mario,inglese, '3384255568', '83040', 'via piani', 22, 'italia', 'roma', 'aaa',0); 
 
-INSERT INTO utente (email, telefono, cap, via, n_civico, nazione, citta, password_utente) 
-VALUES ('b@gmail.com', '3287244801', '82010', 'via alo moro', 12, 'italia', 'milano', 'bbb'); 
+INSERT INTO utente (email,nome,cognome, telefono, cap, via, n_civico, nazione, citta, password_utente,isAdmin) 
+VALUES ('b@gmail.com',luca, rossi, '3287244801', '82010', 'via alo moro', 12, 'italia', 'milano', 'bbb',0); 
 
-INSERT INTO utente (email, telefono, cap, via, n_civico, nazione, citta, password_utente) 
-VALUES ('c@gmail.com', '3216239801', '83510', 'via rossi', 12, 'italia', 'napoli', 'ccc'); 
+INSERT INTO utente (email,nome,cognome, telefono, cap, via, n_civico, nazione, citta, password_utente,isAdmin)  
+VALUES ('c@gmail.com',giacomo,verdi, '3216239801', '83510', 'via rossi', 12, 'italia', 'napoli', 'ccc',0); 
 
-CREATE TABLE amministratore(
-	utente varchar(50) not null primary key,
-    FOREIGN KEY (utente) references utente(email)
-    ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-INSERT INTO amministratore (utente) 
-VALUES ('b@gmail.com');
+INSERT INTO utente (email,nome,cognome, telefono, cap, via, n_civico, nazione, citta, password_utente,isAdmin)  
+VALUES ('admin@gmail.com',marco,bruno, '3216239801', '83510', 'via rossi', 12, 'italia', 'napoli', 'ccc',1); 
 
 
-CREATE TABLE cliente (
-	utente varchar(50) not null primary key, 
-	nome VARCHAR(20) NOT NULL,
-	cognome VARCHAR(20) NOT NULL,
-	FOREIGN KEY (utente) references utente(email)
-    ON DELETE CASCADE ON UPDATE CASCADE
-  );
-  
-  
-INSERT INTO cliente (utente, nome, cognome) 
-VALUES ('a@gmail.com', 'mario', 'rossi');
 
-INSERT INTO cliente (utente, nome, cognome) 
-VALUES ('c@gmail.com', 'giovanni', 'bianchi');
 
   CREATE TABLE categoria (
   nome VARCHAR(45) NOT NULL,
@@ -88,29 +72,94 @@ VALUES ('farine', 'farine', NULL);
   ) AUTO_INCREMENT = 1;
   
   
-INSERT INTO prodotto (codice, nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
-VALUES ('00001', 'set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ( 'zucchine sottolio', 'sei bicchieri', 1.99, 0.435, 3, 'conserve');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ( 'set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'conserve');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'conserve');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'conserve');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'conserve');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'conserve');
+
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'cereali');
   
-INSERT INTO prodotto (codice, nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
-VALUES ('00002', 'oliera', 'per condire gli alimenti', 5.53, 0.999, 5, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'farine');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'farine');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'farine');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'farine');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'farine');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'farine');
 
-INSERT INTO prodotto (codice, nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
-VALUES ('00003', 'set piatti', 'set di sei piatti', 18.35, 2.320, 12, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'legumi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'legumi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'legumi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'legumi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'legumi');
 
-INSERT INTO prodotto (codice, nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
-VALUES ('00004', 'concime', 'concime organico liquido', 7.97, 1.460, 3, 'cereali');
-  
-INSERT INTO prodotto (codice, nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
-VALUES ('00005', 'pattumiera', '100 litri con ruote', 32.60, 8.290, 14, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'miele');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'miele');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'miele');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'miele');
 
-INSERT INTO prodotto (codice, nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
-VALUES ('00006', 'telecamera sorveglianza', 'per esterni', 140.60, 0.222, 22, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'semi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'semi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'semi');
 
-INSERT INTO prodotto (codice, nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
-VALUES ('00007', 'felpa adidas', 'felpa adidas colore rosso', 65.40, 0.312, 5, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'aromi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'aromi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'aromi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'aromi');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'aromi');
 
-INSERT INTO prodotto (codice, nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
-VALUES ('00008', 'jeans', 'jeans colore blu', 51.40, 0.412, 15, 'cereali');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'frutta');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria) 
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'frutta');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'frutta');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'frutta');
+
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'fruttaSecca');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'fruttaSecca');
+INSERT INTO prodotto ( nome, descrizione, prezzo, peso, pezzi_disponibili, categoria)  
+VALUES ('set bicchieri', 'sei bicchieri', 4.99, 0.435, 3, 'fruttaSecca');
 
   CREATE TABLE ordine (
   codice INT NOT NULL AUTO_INCREMENT,
@@ -122,7 +171,7 @@ VALUES ('00008', 'jeans', 'jeans colore blu', 51.40, 0.412, 15, 'cereali');
   cliente VARCHAR(50) NOT NULL,
   prezzo FLOAT NOT NULL,
   PRIMARY KEY (codice),
-  FOREIGN KEY (cliente) REFERENCES cliente(utente)
+  FOREIGN KEY (cliente) REFERENCES utente(email)
   ON DELETE CASCADE ON UPDATE CASCADE
   ) AUTO_INCREMENT = 1;
   
@@ -160,7 +209,7 @@ VALUES (3, 1, '00005');
 
 CREATE TABLE carrello (
  cliente varchar(50) NOT NULL PRIMARY KEY,
- FOREIGN KEY (cliente) REFERENCES cliente(utente)
+ FOREIGN KEY (cliente) REFERENCES utente(email)
  ON DELETE CASCADE ON UPDATE CASCADE
 );
 
