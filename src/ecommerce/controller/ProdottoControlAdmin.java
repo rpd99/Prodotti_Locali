eee93dc7da082c3fb7b0ef562af57c1e902c0bad
raffaele <raffaelmito99@live.it>
2019-07-14 13:31:00 +0200

    gg

--- src/ecommerce/controller/ProdottoControlAdmin.java
+++ src/ecommerce/controller/ProdottoControlAdmin.java
@@ -23,112 +23,119 @@ import ecommerce.utils.Validator;
 public class ProdottoControlAdmin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	enum state {
+		  NO_ACTION,
+		  INVALID_FORM,
+		  ACTION_DELETE
+		}
+	
 	static CategoriaDAO modelCategoria = new CategoriaDAO();
 	static ProdottoDAO modelProdotto = new ProdottoDAO();
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		state flag = state.NO_ACTION;
 		RequestDispatcher dispatcher = null;
+		String cat = request.getParameter("cat");
+		
+		try {
+			if(action != null) {
+				if(action.equalsIgnoreCase("update")) {
+					String nome = request.getParameter("nome");
+					String descrizione = request.getParameter("descrizione");
+					
+					Categoria bean = new Categoria();
+					bean.setNome(nome);
+					bean.setDescrizione(descrizione);
+					
+					modelCategoria.doUpdate(bean);
+					
+					String urlPhoto = (String) request.getParameter("urlPhoto");
+					if (urlPhoto != null) {
+						try {
+							PhotoControl.upload(nome, urlPhoto, "categoria", "nome");
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
+				} else if(action.equalsIgnoreCase("insert")) {
+					String nome = request.getParameter("nome");
+					String descrizione = request.getParameter("descrizione");
+					
+					
+					if(!Validator.isValidFloat(request.getParameter("prezzo"))) {
+						request.setAttribute("formError","Errore prezzo");
+						flag = state.INVALID_FORM;
+					}
+					if(!Validator.isValidFloat(request.getParameter("peso"))) {
+						request.setAttribute("formError","Errore peso");
+						flag = state.INVALID_FORM;
+					}
+					if(!Validator.isValidInt(request.getParameter("pezzi"))) {
+						request.setAttribute("formError","Errore pezzi");
+						flag = state.INVALID_FORM;
+					}
+					
+					
+					if(flag != state.INVALID_FORM) {
+						float prezzo = Float.parseFloat(request.getParameter("prezzo"));
+						float peso = Float.parseFloat(request.getParameter("peso"));
+						int pezzi = Integer.parseInt(request.getParameter("pezzi"));
+						
+						Prodotto bean = new Prodotto();
+						bean.setNome(nome);
+						bean.setDescrizione(descrizione);
+						bean.setPrezzo(prezzo);
+						bean.setPeso(peso);
+						bean.setPezzi_disponibili(pezzi);
+						bean.setCategoria(request.getParameter("cat"));
+						
+						try {
+							modelProdotto.doSave(bean);
+						} catch (SQLIntegrityConstraintViolationException e) {
+							request.setAttribute("formError","Esiste già un prodotto con questo nome");
+							flag = state.INVALID_FORM;
+						}
+						
+						String urlPhoto = (String) request.getParameter("urlPhoto");
+						if (urlPhoto != null) {
+							try {
+								PhotoControl.upload(nome, urlPhoto, "prodotto", "nome");
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}
+						}
+						
+						if (flag != state.INVALID_FORM)
+							request.setAttribute("formSuccess","Prodotto inserito con successo");
+					}
+				} else if(action.equalsIgnoreCase("delete")) {
+					String nome = request.getParameter("cat");
+					modelCategoria.doDelete(nome);
+					flag = state.ACTION_DELETE;
+				}
+			}
+		}  catch(NumberFormatException e) {
+			System.out.println("Error: " + e.getMessage());
+			request.setAttribute("error", e.getMessage());
+		}
 		
 		request.removeAttribute("categories");
 		request.setAttribute("categories", modelCategoria.doRetrieveAll());
 		
 		request.removeAttribute("products");
-		String cat = request.getParameter("cat");
 		request.setAttribute("products", modelProdotto.doRetrieveByCategoria(cat));
-		
-		String action = request.getParameter("action");
-		if(action != null) {
-			if(action.equalsIgnoreCase("update")) {
-				String nome = request.getParameter("nome");
-				String descrizione = request.getParameter("descrizione");
-				
-				Categoria bean = new Categoria();
-				bean.setNome(nome);
-				bean.setDescrizione(descrizione);
-				
-				modelCategoria.doUpdate(bean);
-				
-				String urlPhoto = (String) request.getParameter("urlPhoto");
-				if (urlPhoto != null) {
-					try {
-						PhotoControl.upload(nome, urlPhoto, "categoria", "nome");
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-				request.setAttribute("categories", modelCategoria.doRetrieveAll());
-			} else if(action.equalsIgnoreCase("insert")) {
-				String nome = request.getParameter("nome");
-				String descrizione = request.getParameter("descrizione");
-				
-				boolean IS_VALID_FORM=true;
-				if(!Validator.isValidFloat(request.getParameter("prezzo"))) {
-					request.setAttribute("formError","Errore prezzo");
-					IS_VALID_FORM=false;
-				}
-				if(!Validator.isValidFloat(request.getParameter("peso"))) {
-					request.setAttribute("formError","Errore peso");
-					IS_VALID_FORM=false;
-				}
-				if(!Validator.isValidInt(request.getParameter("pezzi"))) {
-					request.setAttribute("formError","Errore pezzi");
-					IS_VALID_FORM=false;
-				}
-				
-				if(!IS_VALID_FORM) {
-					dispatcher = request.getRequestDispatcher("/adminFilter/categoria-prodotto-admin.jsp?cat="+request.getParameter("cat"));
-					dispatcher.forward(request, response);
-					return;
-				} else {
-					float prezzo = Float.parseFloat(request.getParameter("prezzo"));
-					float peso = Float.parseFloat(request.getParameter("peso"));
-					int pezzi = Integer.parseInt(request.getParameter("pezzi"));
-					
-					Prodotto bean = new Prodotto();
-					bean.setNome(nome);
-					bean.setDescrizione(descrizione);
-					bean.setPrezzo(prezzo);
-					bean.setPeso(peso);
-					bean.setPezzi_disponibili(pezzi);
-					bean.setCategoria(request.getParameter("cat"));
-					
-					try {
-						modelProdotto.doSave(bean);
-					} catch (SQLIntegrityConstraintViolationException e) {
-						request.setAttribute("formError","Esiste già un prodotto con questo nome");
-						dispatcher = this.getServletContext().getRequestDispatcher("/adminFilter/categoria-prodotto-admin.jsp?cat="+request.getParameter("cat"));
-						dispatcher.forward(request, response);
-						return;
-					}
-					
-					String urlPhoto = (String) request.getParameter("urlPhoto");
-					if (urlPhoto != null) {
-						try {
-							PhotoControl.upload(nome, urlPhoto, "prodotto", "nome");
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}
-					}
-					request.setAttribute("products", modelProdotto.doRetrieveByCategoria(cat));
-					request.setAttribute("formSuccess","Prodotto inserito con successo");
-				}
-			} else if(action.equalsIgnoreCase("delete")) {
-				String nome = request.getParameter("cat");
-				modelCategoria.doDelete(nome);
-				request.setAttribute("products", modelProdotto.doRetrieveByCategoria(cat));
-				dispatcher = this.getServletContext().getRequestDispatcher("/adminFilter/gestioneSito.jsp");
-				dispatcher.forward(request, response);
-				return;
-			}
+			
+		if(flag == state.ACTION_DELETE) {
+			dispatcher = this.getServletContext().getRequestDispatcher("/adminFilter/gestioneSito.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			dispatcher = this.getServletContext().getRequestDispatcher("/adminFilter/categoria-prodotto-admin.jsp?cat="+request.getParameter("cat"));
+			dispatcher.forward(request, response);
 		}
-		
-		dispatcher = this.getServletContext().getRequestDispatcher("/adminFilter/categoria-prodotto-admin.jsp?cat="+request.getParameter("cat"));
-		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
